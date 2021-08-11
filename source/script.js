37d4690fd7e697c353ffcd9c1a95cd913d541b00
Ivan Lysukhin <ilysukhin@mail.ru>
2021-08-11 13:49:03 +0300

    modal menu

--- source/script.js
+++ source/script.js
@@ -5,6 +5,10 @@ const page = document.querySelector('html');
 const scrollMenu = document.querySelector('.scroll-menu');
 const searchBtn = document.querySelector('.main-nav__button-link--search');
 const searchModal = document.querySelector('.search-form');
+const burgerBtn = document.querySelector('.main-nav__burger-button');
+const burgerCloseBtn = document.querySelector('.scroll-menu__burger--close');
+const menuModal = document.querySelector('.menu-modal');
+const mainContainer = document.querySelector('.page-main');
 
 const changeClasses = function(elm, id) {
   if (elm.classList.contains('promo-slider__slide--first')) {
@@ -85,13 +89,6 @@ window.addEventListener('scroll', function (evt) {
   }
 });
 
-searchBtn.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  searchModal.style.display = 'block';
-  window.addEventListener('keydown', closeEsc);
-  searchBtn.classList.add('main-nav__button-link--current');
-})
-
 const closeEsc = function (evt) {
   if (evt.keyCode === 27) {
     searchModal.style.display = 'none';
@@ -99,3 +96,30 @@ const closeEsc = function (evt) {
     searchBtn.classList.remove('main-nav__button-link--current');
   }
 };
+
+searchBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  searchModal.style.display = 'block';
+  window.addEventListener('keydown', closeEsc);
+  searchBtn.classList.add('main-nav__button-link--current');
+})
+
+const closeModalByEsc = function (evt) {
+  if (evt.keyCode === 27) {
+    menuModal.style.display = 'none';
+    window.removeEventListener('keydown', closeModalByEsc);
+    mainContainer.style.display = 'grid';
+  }
+};
+
+burgerBtn.addEventListener('click', function (evt) {
+  menuModal.style.display = 'block';
+  mainContainer.style.display = 'none';
+  window.addEventListener('keydown', closeModalByEsc)
+});
+
+burgerCloseBtn.addEventListener('click', function (evt) {
+  menuModal.style.display = 'none';
+  mainContainer.style.display = 'grid';
+  window.removeEventListener('keydown', closeModalByEsc);
+});
